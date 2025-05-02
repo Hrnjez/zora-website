7822370ea96f63185ed93e6687e691d906cafd34
Hrnjez <54723911+Hrnjez@users.noreply.github.com>
2025-05-02 18:07:48 +0200

    Update get-balances.js

--- api/get-balances.js
+++ api/get-balances.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     // Fetch balances (first page only)
     const balancesResponse = await getProfileBalances({
       identifier: address,
-      count: 50,
+      count: 99,
     });
 
     res.status(200).json({
